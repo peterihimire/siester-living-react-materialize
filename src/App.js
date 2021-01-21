bae3b874969c27a6a8be9b2d03a9eb316c384028
Peter Ihimire <peterihimire@gmail.com>
2021-01-21 19:32:38 +0100

    Working on the available space component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Banner from "./components/Banner";
+import AvailableSpace from "./components/AvailableSpace";
 
 function App() {
   return (
     <>
       <Navbar />
       <Banner />
+      <AvailableSpace />
     </>
   );
 }
---
+++ src/components/AvailableSpace.js
@@ -0,0 +1,44 @@
+import React from "react";
+import "./AvailableSpace.css";
+import spaceOne from "../assets/space-1.png";
+import spaceTwo from "../assets/space-2.png";
+import spaceThree from "../assets/space-3.png";
+
+const AvailableSpace = () => {
+  return (
+    <section id="available-space">
+      <div className="container">
+        <div className="row">
+          <div className="col s12">
+            <div className="available-spaces-container">
+              <div className="available-header">
+                <h4>Check out our available space</h4>
+              </div>
+              <div className="col s12 m4 l4">
+                <div className="space-img">
+                  <img src={spaceOne} alt="home interior" />
+                </div>
+              </div>
+              <div className="col s12 m4 l4">
+                <div className="space-img">
+                  <img src={spaceTwo} alt="home interior" />
+                </div>
+              </div>
+              <div className="col s12 m4 l4">
+                <div className="space-img">
+                  <img src={spaceThree} alt="home interior" />
+                </div>
+              </div>
+              <div className="available-info">
+                <p>Want more information ? Call 08060958134</p>
+                <button className="btn available-btn">book now</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AvailableSpace;
