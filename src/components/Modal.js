198766d98dd70c37bd495bcdcc8227cbd8e85d5f
Peter Ihimire <peterihimire@gmail.com>
2021-01-26 23:09:21 +0100

    Modified the Modal component by changing it to class based component so that I can make use of the modal features

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,9 +1,12 @@
-import React from "react";
-import "./Modal.css";
+import React, { Component } from "react";
 import M from "materialize-css/dist/js/materialize.min.js";
+// import logo from "../assets/logo.svg";
+// import logoAlt from "../assets/logo-alt.svg";
+// import x from "../assets/x.svg";
+import "./Modal.css";
 
-const Modal = () => {
-  React.useEffect(() => {
+class Modal extends Component {
+  componentDidMount() {
     const options = {
       onOpenStart: () => {
         console.log("Open Start");
@@ -24,23 +27,30 @@ const Modal = () => {
       startingTop: "4%",
       endingTop: "10%",
     };
-    M.Modal.init(Modal, options);
-  }, []);
-  return (
-    <>
-      <div
-        ref={(Modal) => {
-          Modal = Modal;
-        }}
-        id="modal-select"
-        className="modal"
-      >
-        <div className="modal-content">
-          <h3>This is the modal</h3>
+    M.Modal.init(this.Modal, options);
+    // If you want to work on instance of the Modal then you can use the below code snippet
+    // let instance = M.Modal.getInstance(this.Modal);
+    // instance.open();
+    // instance.close();
+    // instance.destroy();
+  }
+  render() {
+    return (
+      <>
+        <div
+          ref={(Modal) => {
+            this.Modal = Modal;
+          }}
+          id="modal-select"
+          className="modal"
+        >
+          <div className="modal-content">
+            <h3>This is the modal</h3>
+          </div>
         </div>
-      </div>
-    </>
-  );
-};
+      </>
+    );
+  }
+}
 
 export default Modal;
